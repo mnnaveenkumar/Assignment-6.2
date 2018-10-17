fabcdf85f458abfd285f2917d35a59dbe4cf8caa
mnnaveenkumar <36623132+mnnaveenkumar@users.noreply.github.com>
2018-10-17 14:59:52 +0530

    Final Commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var recipeName = "Creamy squash linguine";
+var preparationTime = 45;
+var recipeBy = "Jimmy Cutter";
+var isVegetarian = true;
+var calories = 223.7;
+
+console.log("Recipe Details");
+console.log('--------------')
+console.log("Recipe Name : " + recipeName);
+console.log("Preparation Time : " + preparationTime);
+console.log("Recipe By : " + recipeBy);
+console.log("is Vegetarian Dish : " + isVegetarian);
+console.log("Calories : "+ calories);
+
+preparationTime = 60;
+recipeBy = "Jimmy Cutter George";
+
+console.log("\nRecipe Details after update");
+console.log('---------------------------')
+console.log("Recipe Name : " + recipeName);
+console.log("Preparation Time : " + preparationTime);
+console.log("Recipe By : " + recipeBy);
+console.log("is Vegetarian Dish : " + isVegetarian);
+console.log("Calories : "+ calories);
